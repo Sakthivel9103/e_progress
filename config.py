af90491af396bfb4dee9d5d8f92813d410b9ec71
nalinimanogaran614-alt <nalinimanogaran614@gmail.com>
2026-09-10 05:36:14 +0545

    Support Railway MySQL environment variables

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 
 import os
 
-DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_USER = os.environ.get("DB_USER", "e_progress_app")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "EProgress_Local_2026_App")
-DB_NAME = os.environ.get("DB_NAME", "e_progress_card")
-DB_PORT = int(os.environ.get("DB_PORT") or "3306")
+DB_HOST = os.environ.get("DB_HOST") or os.environ.get("MYSQLHOST", "localhost")
+DB_USER = os.environ.get("DB_USER") or os.environ.get("MYSQLUSER", "e_progress_app")
+DB_PASSWORD = os.environ.get("DB_PASSWORD") or os.environ.get("MYSQLPASSWORD", "EProgress_Local_2026_App")
+DB_NAME = os.environ.get("DB_NAME") or os.environ.get("MYSQLDATABASE", "e_progress_card")
+DB_PORT = int(os.environ.get("DB_PORT") or os.environ.get("MYSQLPORT") or "3306")
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key-in-production")
